4a5ad78f9403433af2dace1348e4ea9ae6a0cd6b
atakan1001 <atakan_arslan1001@hotmail.com>
2024-03-19 21:53:08 +0300

    deneme

--- src/main/java/org/waterfall/Main.java
+++ src/main/java/org/waterfall/Main.java
@@ -3,5 +3,6 @@ package org.waterfall;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Atakan");
     }
 }
