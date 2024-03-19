7a5426564ca7f71a2230cffb2c076457aee2afd0
kaganhitit11 <ohitit20@ku.edu.tr>
2024-03-19 22:31:34 +0300

    sebnem added

--- src/main/java/org/waterfall/Main.java
+++ src/main/java/org/waterfall/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Atakan");
         System.out.println("İdil");
+        System.out.println("Sebnem");
     }
 }
