3ed1ddcbed2ab1ea103d3bcda7310a2584bd3444
kaganhitit11 <ohitit20@ku.edu.tr>
2024-03-19 22:05:16 +0300

    secondary commit

--- src/main/java/org/waterfall/Main.java
+++ src/main/java/org/waterfall/Main.java
@@ -2,6 +2,6 @@ package org.waterfall;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Oguz Yazdı2");
     }
 }
