ccf9acd37fc69c6dd64ad05ca72b5a3a8411c2a5
idil-gorgulu <igorgulu21@ku.edu.tr>
2024-04-03 11:58:39 +0300

    MagicalStaffController created

---
+++ src/main/java/org/Controllers/MagicalStaffController.java
@@ -0,0 +1,17 @@
+package org.Controllers;
+
+import org.Domain.MagicalStaff;
+import org.Views.RunningModePage;
+import org.Domain.Game;
+
+public class MagicalStaffController {
+    private Game game;
+    private RunningModePage runningModePage;
+    MagicalStaff magicalStaff;
+
+    public MagicalStaffController(RunningModePage runningModePage){
+        this.magicalStaff = new MagicalStaff();
+    }
+
+
+}
