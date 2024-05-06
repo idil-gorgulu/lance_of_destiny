a3d394561022680f5545d9b7fcde4bad96814f4e
SebnemDemirtas <sdemirtas20@ku.edu.tr>
2024-05-06 22:30:20 +0300

    bug solved (was accidentally creating new game instance)

--- src/main/java/org/Controllers/DataBaseController.java
+++ src/main/java/org/Controllers/DataBaseController.java
@@ -13,7 +13,6 @@ public class DataBaseController {
     private static Game gameSession;
     private static DataBaseController instance;
     public DataBaseController(){
-
         this.gameSession = Game.getInstance();
     }
 
