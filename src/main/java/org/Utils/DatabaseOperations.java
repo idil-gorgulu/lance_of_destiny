51d332de6f655bdfa3e5e87754e42177a22d2b42
atahalicioglu <atahalicioglu@gmail.com>
2024-05-07 10:22:07 +0300

    deleted all game templates

---
+++ src/main/java/org/Utils/DatabaseOperations.java
@@ -0,0 +1,17 @@
+package org.Utils;
+
+import org.bson.Document;
+
+public class DatabaseOperations {
+    // This class is for single Database operations, such as deleting all the instances, etc.
+    // DO NOT USE THIS CLASS TO IMPLEMENT ANY GAME FUNCTION
+    Database db = Database.getInstance();
+
+    public void deleteAllInstances() {
+        db.getGameCollection().deleteMany(new Document());
+    }
+
+    public static void main(String[] args) {
+        // Do the operation you want to do on DB
+    }
+}
