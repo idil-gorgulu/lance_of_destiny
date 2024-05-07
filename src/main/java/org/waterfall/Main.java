80c0ab68545845f03d89798021809cdf0e5cadc4
atahalicioglu <atahalicioglu@gmail.com>
2024-05-07 09:58:21 +0300

    single user for testing purposes

--- src/main/java/org/waterfall/Main.java
+++ src/main/java/org/waterfall/Main.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        /*
+        // COMMENTED OUT FOR ONLY 1 USER PURPOSES
         // Database initialized in here
         Database dbSingleton = Database.getInstance();
         User user = User.getUserInstance();
@@ -15,6 +17,16 @@ public class Main {
             navigator.showEnterPage();
             navigator.show();
         });
-    }
+        */
 
+        Database dbSingleton = Database.getInstance();
+        User user = User.getUserInstance();
+        user.setEmail("deneme");
+        SwingUtilities.invokeLater(() -> {
+            Navigator navigator = Navigator.getInstance();
+            navigator.showGameModePage();
+            navigator.show();
+        });
+
+    }
 }
